17b6882c74c8b778767bcd108a70a85134ba4d81
wonniiii <eve652358@gmail.com>
2023-05-25 16:46:49 +0900

    Initial Commit

---
+++ Keyneez-iOS-Release/Keyneez_iOS_ReleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Keyneez_iOS_ReleaseApp.swift
+//  Keyneez-iOS-Release
+//
+//  Created by 최효원 on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Keyneez_iOS_ReleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
