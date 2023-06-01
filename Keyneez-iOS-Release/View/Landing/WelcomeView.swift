0aaafab129421525d2523af5181b352418f162b7
Dohyun Chung <pastapeter@naver.com>
2023-06-02 00:49:51 +0900

    [Chore] WelcomeView move

---
+++ Keyneez-iOS-Release/View/Landing/WelcomeView.swift
@@ -0,0 +1,44 @@
+//
+//  WelcomeView.swift
+//  Keyneez-iOS-Release
+//
+//  Created by Jung peter on 6/1/23.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+      ZStack {
+        Image("background")
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .frame(minWidth: 0, maxWidth: .infinity)
+          .edgesIgnoringSafeArea(.all)
+        VStack {
+          Spacer()
+            .frame(height: 51)
+         Image("logoA")
+          Spacer()
+          Text("GIF 위치")
+            .frame(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.width / 2)
+            .border(.black)
+          Spacer()
+          Button(action: {}) {
+            Image("kakaoLogin")
+          }
+          Button(action: {}) {
+            Image("appleLogin")
+          }
+        }
+      }
+      .padding(.horizontal)
+    }
+    
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
