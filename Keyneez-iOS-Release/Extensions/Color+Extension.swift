3e619ac9c625153199867c50b94245e81bab2509
wonniiii <eve652358@gmail.com>
2023-06-20 19:26:42 +0900

    [Add] Color, Font 추가

---
+++ Keyneez-iOS-Release/Extensions/Color+Extension.swift
@@ -0,0 +1,44 @@
+//
+//  Color+Extension.swift
+//  Keyneez-iOS-Release
+//
+//  Created by 최효원 on 2023/06/11.
+//
+
+import SwiftUI
+
+extension Color {
+  
+  static let mint600 = Color("mint600")
+  static let mint500 = Color("mint500")
+  static let mint400 = Color("mint400")
+  static let mint300 = Color("mint300")
+  static let mint200 = Color("mint200")
+
+  static let red600 = Color("red600")
+  static let red500 = Color("red500")
+  static let red400 = Color("red400")
+  static let red300 = Color("red300")
+  static let red200 = Color("red200")
+  
+  static let purple600 = Color("purple600")
+  static let purple500 = Color("purple500")
+  static let purple400 = Color("purple400")
+  static let purple300 = Color("purple300")
+  static let purple200 = Color("purple200")
+  
+  static let gray900 = Color("gray900")
+  static let gray800 = Color("gray800")
+  static let gray700 = Color("gray700")
+  static let gray600 = Color("gray600")
+  static let gray500 = Color("gray500")
+  static let gray400 = Color("gray400")
+  static let gray300 = Color("gray300")
+  static let gray200 = Color("gray200")
+  static let gray100 = Color("gray100")
+  static let gray050 = Color("gray050")
+
+
+
+
+}
