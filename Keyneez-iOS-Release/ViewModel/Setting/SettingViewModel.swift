d34c4765719efbb1326c6f3e92e79ae03d8bae40
kpk0616 <kpk0616@naver.com>
2023-07-03 19:26:19 +0900

    [Add] #3 - 설정 뷰모델 추가

---
+++ Keyneez-iOS-Release/ViewModel/Setting/SettingViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  SettingViewModel.swift
+//  Keyneez-iOS-Release
+//
+//  Created by 박의서 on 2023/06/21.
+//
+
+import SwiftUI
+
+struct SettingViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingViewModel()
+    }
+}
