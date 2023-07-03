23442e9c786d49247ca0967981c8286f1d79760d
kpk0616 <kpk0616@naver.com>
2023-07-03 23:45:46 +0900

    [Feat] #3 - 상세페이지 뷰 스크롤뷰와 버튼 분리

--- Keyneez-iOS-Release/View/Component/ButtonComponent.swift
+++ Keyneez-iOS-Release/View/Component/ButtonComponent.swift
@@ -5,4 +5,39 @@
 //  Created by 박의서 on 2023/07/03.
 //
 
-import Foundation
+import SwiftUI
+
+struct CancelButtonStyle: ButtonStyle {
+    @State private var fontSize: CGFloat = 18
+    @State private var pressed = false
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: fontSize))
+            .foregroundColor(.white) // TODO: gray500
+            .padding()
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .foregroundColor(.white)
+            .background(RoundedRectangle(cornerRadius: 12.0).fill(Color.gray) // TODO: gray100
+                        
+            )
+    }
+}
+
+struct BlackButtonStyle: ButtonStyle {
+    @State private var fontSize: CGFloat = 18
+    @State private var pressed = false
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: fontSize))
+            .foregroundColor(.white) // TODO: gray500
+            .padding()
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .foregroundColor(.white)
+            .background(RoundedRectangle(cornerRadius: 12.0).fill(Color.black) // TODO: gray100
+                        
+            )
+    }
+}
+
--- Keyneez-iOS-Release/View/DetailView.swift
+++ Keyneez-iOS-Release/View/DetailView.swift
@@ -10,35 +10,54 @@ import SwiftUI
 struct DetailView: View {
     var body: some View {
         ZStack {
-            Color.gray.ignoresSafeArea()
-            ZStack {
-                RoundedRectangle(cornerRadius: 30)
-                    .foregroundColor(.white)
-                    .padding(.top, 286)
+            ScrollView {
+                ZStack {
+                    VStack {
+                        Image("Detail_Activity_01") // TODO: - 태그에 따라 이미지 변경
+                            .scaledToFit()
+                        Spacer()
+                    }
+                    .ignoresSafeArea()
+                    RoundedRectangle(cornerRadius: 60)
+                        .foregroundColor(.gray)
+                        .padding(.top, 286)
+                        .ignoresSafeArea()
+                        .scaledToFit()
+                // TODO: - Rectangle 어디까지??
+                }
             }
-            
+            .ignoresSafeArea()
             
             // button
-            VStack {
-                Spacer()
-                Button {
-                    // action
-                } label: {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 14)
-                            .frame(height: 56)
-                            .padding(22)
-                            .foregroundColor(.black)
-                        Text("신청하러 가기")
-                            .foregroundColor(.white)
+            ZStack {
+                VStack {
+                    Spacer()
+                    Rectangle()
+                        .frame(height: 123)
+                        .foregroundColor(.white)
+                        .mask(LinearGradient(gradient: Gradient(colors: [.black, .black, .black, .clear]), startPoint: .bottom, endPoint: .top))
+                    // TODO: - 그라데이션 지우면 heigt을 107로 수정
+                }
+                
+                VStack {
+                    Spacer()
+                    Button {
+                        // action
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 14)
+                                .frame(height: 56)
+                                .padding(34)
+                                .foregroundColor(.black)
+                            Text("신청하러 가기")
+                                .foregroundColor(.white)
+                            // TODO: URL 연결
+                        }
                     }
                 }
             }
-            
+            .ignoresSafeArea()
         }
-//        ScrollView {
-//            Image("DetailView")
-//        }.ignoresSafeArea()
     }
 }
 
