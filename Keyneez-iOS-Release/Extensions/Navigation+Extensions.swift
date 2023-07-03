f8e2da924945d8c834eaf1bc2910de904b2b2c31
kpk0616 <kpk0616@naver.com>
2023-07-03 19:25:45 +0900

    [Feat] #3 - 네비게이션바 Extension 추가

---
+++ Keyneez-iOS-Release/Extensions/Navigation+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Keyneez-iOS-Release
+//
+//  Created by 박의서 on 2023/06/22.
+//
+
+import SwiftUI
+
+extension UINavigationController {
+    // Remove back button text
+    open override func viewWillLayoutSubviews() {
+        navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+}
