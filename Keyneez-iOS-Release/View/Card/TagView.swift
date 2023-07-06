4d33ea9c93fcf12d5c220baa961000032ff2d0a2
wonniiii <eve652358@gmail.com>
2023-07-07 00:02:43 +0900

    [Feat] 카드 태그 뷰 생성

---
+++ Keyneez-iOS-Release/View/Card/TagView.swift
@@ -0,0 +1,20 @@
+//
+//  TagView.swift
+//  Keyneez-iOS-Release
+//
+//  Created by 최효원 on 2023/07/07.
+//
+
+import SwiftUI
+
+struct TagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TagView_Previews: PreviewProvider {
+    static var previews: some View {
+        TagView()
+    }
+}
