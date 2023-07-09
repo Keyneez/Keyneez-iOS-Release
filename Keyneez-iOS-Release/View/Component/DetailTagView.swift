92dbb84e81cbc7660c5dd94e0e290db9df3f0e55
kpk0616 <kpk0616@naver.com>
2023-07-09 17:40:45 +0900

    [Del] #3 - DetailContentView 삭제

---
+++ Keyneez-iOS-Release/View/Component/DetailTagView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailTagView.swift
+//  Keyneez-iOS-Release
+//
+//  Created by 박의서 on 2023/07/09.
+//
+
+import SwiftUI
+
+struct DetailTagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailTagView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailTagView()
+    }
+}
