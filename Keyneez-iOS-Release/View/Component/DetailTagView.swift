65601823023cb4bce5bcb0f7a0d02e283eb3957d
kpk0616 <kpk0616@naver.com>
2023-07-12 23:14:59 +0900

    [Chore] #3 - DetailTagState Model로 파일 분리

--- Keyneez-iOS-Release/View/Component/DetailTagView.swift
+++ Keyneez-iOS-Release/View/Component/DetailTagView.swift
@@ -7,20 +7,6 @@
 
 import SwiftUI
 
-enum DetailTagState {
-    case hobby // 취미
-    case culture // 문화생활
-    case course // 진로
-    case speech // 강연
-    case lecture // 강의
-    case mentoring // 멘토링
-    case activity // 활동
-    case supporters // 서포터즈
-    case club // 동아리
-    case campaign // 캠페인
-    case competition // 공모전
-}
-
 struct DetailTagView: View {
     private var tag: DetailTagState
     
