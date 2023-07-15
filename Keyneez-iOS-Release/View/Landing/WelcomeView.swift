e8bf41ae789a6237bfe88c51fc8f3b9296773c5d
Dohyun Chung <pastapeter@naver.com>
2023-07-16 01:58:47 +0900

    [Feat] WelcomeView

--- Keyneez-iOS-Release/View/Landing/WelcomeView.swift
+++ Keyneez-iOS-Release/View/Landing/WelcomeView.swift
@@ -7,46 +7,78 @@
 
 import SwiftUI
 
-
 struct WelcomeView: View {
+  
+  @StateObject var viewModel: WelcomeViewModel
     
   var welcomeTexts = ["청소년들에게 필요한\n활동과 혜택 정보를 한눈에!", "다양한 세상을 키니즈와 함께 둘러볼까요?"]
  @State var page = 0
+ @State var isShowingNext = true
   
-    var body: some View {
-      ZStack {
-        Image("background")
-          .resizable()
-          .aspectRatio(contentMode: .fill)
-          .frame(minWidth: 0, maxWidth: .infinity)
-          .edgesIgnoringSafeArea(.all)
-        VStack {
-          Spacer()
-            .frame(height: 51)
-         Image("logoA")
-          Spacer()
-          Text("GIF 위치")
-            .frame(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.width / 2)
-            .border(.black)
-          Text(welcomeTexts[0])
-            .multilineTextAlignment(.center)
-            .font(.system(size: 20 * 3/4, weight: .bold))
-          pageControl
-          Spacer()
-          Button(action: {
-            
-          }) {
-            Image("kakaoLogin")
-          }
-          Button(action: {}) {
-            Image("appleLogin")
-          }
-          Spacer()
-            .frame(height: 20)
-        }
+  var body: some View {
+    ZStack {
+      Image("background")
+        .resizable()
+        .aspectRatio(contentMode: .fill)
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .edgesIgnoringSafeArea(.all)
+      if viewModel.isLoading {
+        ProgressView()
+          .scaleEffect(2)
+      }
+      VStack {
+        Spacer()
+          .frame(height: 51)
+        Image("logoA")
+        Spacer()
+        Text("GIF 위치")
+          .frame(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.width / 2)
+          .border(.black)
+        Text(welcomeTexts[0])
+          .multilineTextAlignment(.center)
+          .font(.system(size: 20 * 3/4, weight: .bold))
+        pageControl
+        Spacer()
+        kakaoButton()
+        appleButton()
+        Spacer()
+          .frame(height: 20)
       }
       .padding(.horizontal)
     }
+  }
+  
+  
+  @ViewBuilder
+  private func appleButton() -> some View {
+    
+    Button {
+      viewModel.didTapAppleLogin()
+    } label: {
+      Image("appleLogin")
+    }
+    .navigationDestination(isPresented: $viewModel.readyToNavigation) {
+      coordinate()
+    }
+    
+  }
+  
+  
+  @ViewBuilder
+  private func kakaoButton() -> some View {
+    
+    Button {
+      viewModel.didTapLoginWithKakao()
+    } label: {
+      Image("kakaoLogin")
+    }
+    .navigationDestination(isPresented: $viewModel.readyToNavigation) {
+      coordinate()
+    }
+    
+
+  }
+  
   
   @ViewBuilder
   private var pageControl: some View {
@@ -54,11 +86,23 @@ struct WelcomeView: View {
       PageIndicator(currentPage: $page, numberOfpages: 3)
     }
   }
+  
+  @ViewBuilder
+  private func coordinate() -> some View {
+    if let nextPage = viewModel.nextPage {
+      switch nextPage {
+      case .signup(let viewModel):
+        RegisterIDView(viewModel: viewModel)
+      case .home:
+        HomeView()
+      }
+    }
+  }
     
 }
 
 struct WelcomeView_Previews: PreviewProvider {
     static var previews: some View {
-        WelcomeView()
+      WelcomeView(viewModel: WelcomeViewModel())
     }
 }
--- Keyneez-iOS-Release/View/Landing/WelcomeViewModel.swift
+++ Keyneez-iOS-Release/View/Landing/WelcomeViewModel.swift
@@ -9,22 +9,61 @@ import Foundation
 import KakaoSDKAuth
 import KakaoSDKUser
 import KakaoSDKCommon
+import Combine
+import SwiftUI
 
-final class WelcomeViewModel {
+enum WelcomeViewModelError: Error {
+  case kakaoLoginNotAvailable
+}
+
+enum Page {
+  case signup(viewModel: RegisterIDViewModel)
+  case home
+}
+
+final class WelcomeViewModel: ObservableObject {
   
-  func loginWithKakao() {
-    
-    if UserApi.isKakaoTalkLoginAvailable() {
-      UserApi.shared.loginWithKakaoTalk { token, error in
-        // kakao 토큰 서버로 전달
-      }
-    } else {
-      UserApi.shared.loginWithKakaoAccount { token, error in
-        // kakao Token 서버로 전달
-      }
-      
-    }
+  @Published var nextPage: Page? = nil
+  @Published var readyToNavigation = false
+  @Published var isLoading = false
+  
+  private var repository: OAuthRepositoryProtocol
+  
+  init() {
+    repository = OAuthRepository()
   }
   
-  
+  func didTapAppleLogin() {
+    
+  }
+
+  func didTapLoginWithKakao() {
+    do {
+      Task {
+        await MainActor.run {
+          isLoading = true
+        }
+        let responseAndToken = try await repository.loginWithKakao()
+        let response = responseAndToken.dto
+        
+        let idToken = responseAndToken.idToken
+        let kakaoAccessToken = responseAndToken.accessToken
+        
+        await MainActor.run {
+          nextPage = .signup(viewModel: RegisterIDViewModel(idToken: idToken, kakaoAccessToken: kakaoAccessToken))
+          readyToNavigation = true
+          isLoading = false
+        }
+      }
+    }
+    catch(let error){
+      
+    }
+    
+  }
+
 }
+
+
+
+
