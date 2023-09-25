c861f7d851e462f730e43441d7e319cdf09585dd
kpk0616 <kpk0616@naver.com>
2023-09-25 21:21:22 +0900

    [Feat] #25 - Home뷰 레이아웃 leading 맞춤

--- Keyneez-iOS-Release/View/Home/HomeView.swift
+++ Keyneez-iOS-Release/View/Home/HomeView.swift
@@ -15,60 +15,69 @@ struct HomeView: View {
   var rows : [GridItem] = Array(repeating: .init(.fixed(220)), count: 1)
   var body: some View {
     NavigationStack {
-      ZStack(alignment: .top) {
-        HomeBackgroundView()
-        VStack(alignment: .leading, spacing: 0) {
-          Spacer().frame(height: 17)
-          HStack {
-            Image("logo")
-            Spacer()
-          }
-          Spacer().frame(height:33)
-          //viewModel 빼기
-          
-          let userName = UserManager.shared.user?.nickName
-          Text(userName ?? "11")
-            .font(.pretendard(.bold, size: 24))
-            .foregroundColor(.gray900)
-          +
-          Text("님을 위한")
-            .font(.pretendard(.semiBold, size: 24))
-          //            JellyView()
-          WeekRecommendCollectionView()
-          
-          Spacer().frame(height: 30)
-          NavigationLink(destination: RecentUpdateDetailView()) {
+      ScrollView {
+        ZStack(alignment: .top) {
+          HomeBackgroundView()
+          VStack(alignment: .leading, spacing: 0) {
+            Spacer().frame(height: 17)
             HStack {
-              Text("최근 업데이트")
-                .font(.pretendard(.semiBold, size: 24))
-                .foregroundColor(Color.gray900)
-              Image("ic_detail")
-                .resizable()
-                .frame(width: 5, height: 10)
+              Image("logo")
+              Spacer()
             }
-          }
-          
-          Spacer().frame(height: 19)
-          ScrollView(.horizontal) {
-            LazyHGrid(rows: rows, spacing: 15) {
-              ForEach(allViewModel.allCardList, id: \.contentPk) {content in
-                NavigationLink(destination: DetailView(pk: content.contentPk)) {
-                  HomeCardCell(model: content)
-                }
+            Spacer().frame(height:33)
+            //viewModel 빼기
+            
+            let userName = UserManager.shared.user?.nickName
+            Text(userName ?? "11")
+              .font(.pretendard(.bold, size: 24))
+              .foregroundColor(.gray900)
+            +
+            Text("님을 위한")
+              .font(.pretendard(.semiBold, size: 24))
+            //            JellyView()
+            WeekRecommendCollectionView()
+            
+            Spacer().frame(height: 30)
+            NavigationLink(destination: RecentUpdateDetailView()) {
+              HStack {
+                Text("최근 업데이트")
+                  .font(.pretendard(.semiBold, size: 24))
+                  .foregroundColor(Color.gray900)
+                Image("ic_detail")
+                  .resizable()
+                  .frame(width: 5, height: 10)
               }
             }
+            
+            Spacer().frame(height: 19)
+            ScrollView(.horizontal) {
+              ZStack(alignment: .topLeading) {
+                LazyHGrid(rows: rows, spacing: 15) {
+                  ForEach(allViewModel.allCardList, id: \.contentPk) {content in
+                    NavigationLink(destination: DetailView(pk: content.contentPk)) {
+                      HomeCardCell(model: content)
+                    }
+                  }
+                }
+                TipKitView()
+                  .frame(width: 181, height: 34)
+                  .padding(.leading, 88)
+                  .padding(.top, 36)
+              }
+              .scrollIndicators(.hidden)
+            }
+            .padding([.trailing], 28)
+            Spacer().frame(height: 20)
           }
           .scrollIndicators(.hidden)
+          .onAppear {
+            allViewModel.fetchAllCard(filter: nil)
+            
+          }
+          .padding(.leading, 28)
         }
-        .padding([.leading, .trailing], 28)
-      }
-      .scrollIndicators(.hidden)
-      .onAppear {
-        allViewModel.fetchAllCard(filter: nil)
-        
       }
     }
   }
 }
-
 
